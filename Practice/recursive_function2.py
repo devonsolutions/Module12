b0950beaf3fa140be13f8e7e03606e62406adf77
devonsolutions <cache-out@outlook.com>
2025-11-13 14:30:24 -0800

    Initial commit

---
+++ Practice/recursive_function2.py
@@ -0,0 +1,23 @@
+'''
+Recursive Function
+'''
+##
+# Define function named factorial 
+# @param n
+# The base case is n <= 1: return 1
+# else @var result = n * factorial(n - 1) This is the recursive call
+# return result
+
+#enter syntax here
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        result = n * factorial(n - 1)
+        return result
+
+n = 5
+#Call factorial function and pass n as the argument
+print(f"Factorial of {n} is: {factorial(n)}.")
+#enter syntax here
+
